018afcb6e78374e096db927545a7697355ad107b
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-11-04 02:23:04 -0700

    Added working build and drop tables.

--- src/main.cc
+++ src/main.cc
@@ -65,6 +65,42 @@ int main( int argc, char** argv ){
         string line;
         Connector *connector = new Connector();
 
+    //drop the existing table and build the new ones
+        connector->executeStatement( "DROP TABLE IF EXISTS `geo_ip_location`" );
+        string current_query =
+            "CREATE TABLE `geo_ip_location`("
+                    "`id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,"
+                    "`country` CHAR(2) NOT NULL,"
+                    "`region` CHAR(2),"
+                    "`city` VARCHAR(32),"
+                    "`postal_code` VARCHAR(10),"
+                    "`latitude` DECIMAL(18,12),"
+                    "`longitude` DECIMAL(18,12),"
+                    "`metro_code` SMALLINT,"
+                    "`area_code` SMALLINT,"
+                    "PRIMARY KEY (`id`),"
+                    "INDEX `lat_long_index` ( `latitude`, `longitude` ),"
+                    "INDEX `country_region_city_index` ( `country`, `region`, `city` ),"
+                    "INDEX `postal_code_index` ( `postal_code` ),"
+                    "INDEX `area_code_index` ( `area_code` )"
+            ") ENGINE=MyISAM"
+        ;
+        connector->executeStatement( current_query );
+
+
+        connector->executeStatement( "DROP TABLE IF EXISTS `geo_ip_block`" );
+        current_query =
+            "CREATE TABLE `geo_ip_block`("
+                    "`start_ip` MEDIUMINT UNSIGNED NOT NULL,"
+                    "`end_ip` MEDIUMINT UNSIGNED NOT NULL,"
+                    "`location_id` INTEGER UNSIGNED NOT NULL,"
+                    "INDEX `ip_range_index` ( `start_ip`, `end_ip` ),"
+                    "INDEX `location_index` ( `location_id` )"
+            ") ENGINE=MyISAM"
+        ;
+        connector->executeStatement( current_query );
+
+
     //import the locations section
         ifstream locations_file( location_filename.c_str() );
         const boost::regex locations_pattern( "(\\d+),\"(..)\",\"(..)\",\"(.*?)\",\"(.*?)\",(.*?),(.*?),(\\d+),(\\d+)" );
